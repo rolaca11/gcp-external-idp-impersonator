3b95dddd3063d9d714d4aa20b2734b30dc024828
László Stahorszki <laszlo.stahorszki@ropi.dev>
2023-04-16 17:43:25 +0200

    Linting

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,22 @@
-import {defaultOptions, mergeOptionsWithDefaults} from "./index";
+import { defaultOptions, mergeOptionsWithDefaults } from './index'
 
-test("mergeOptionsWithDefaults fills nullable fields", () => {
-    const result = mergeOptionsWithDefaults({authToken: "asd", serviceAccount: "asd"});
-    expect(result).toHaveProperty("stsEndpoint", defaultOptions.stsEndpoint)
-    expect(result).toHaveProperty("iamAudience", defaultOptions.iamAudience)
+test('mergeOptionsWithDefaults fills nullable fields', () => {
+    const result = mergeOptionsWithDefaults({
+        authToken: 'asd',
+        serviceAccount: 'asd',
+        stsAudience: ''
+    })
+    expect(result).toHaveProperty('stsEndpoint', defaultOptions.stsEndpoint)
+    expect(result).toHaveProperty('iamAudience', defaultOptions.iamAudience)
 })
 
-test("mergeOptionsWithDefaults doesn't override given fields", () => {
-    const result = mergeOptionsWithDefaults({authToken: "asd", serviceAccount: "asd", stsEndpoint: "else"});
-    expect(result).toHaveProperty("stsEndpoint", "else")
-    expect(result).toHaveProperty("iamAudience", defaultOptions.iamAudience)
+test('mergeOptionsWithDefaults doesn\'t override given fields', () => {
+    const result = mergeOptionsWithDefaults({
+        authToken: 'asd',
+        serviceAccount: 'asd',
+        stsEndpoint: 'else',
+        stsAudience: ''
+    })
+    expect(result).toHaveProperty('stsEndpoint', 'else')
+    expect(result).toHaveProperty('iamAudience', defaultOptions.iamAudience)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,56 @@
-export const mergeOptionsWithDefaults = (options: Options) => ({
+export const mergeOptionsWithDefaults = (options: Options): MergedOptions => ({
     ...defaultOptions,
-    ...options,
+    ...options
 })
 const getAccessToken = async (options: Options): Promise<string> => {
     const mergedOptions = mergeOptionsWithDefaults(options)
 
-    const stsToken = await fetch(mergedOptions.stsEndpoint, {
-        method: "POST",
+    const stsToken: string = await fetch(mergedOptions.stsEndpoint, {
+        method: 'POST',
         headers: {
-            "Content-Type": "application/json"
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-                "grantType": "urn:ietf:params:oauth:grant-type:token-exchange",
-                "audience": mergedOptions.stsAudience,
-                "requestedTokenType": "urn:ietf:params:oauth:token-type:access_token",
-                "subjectToken": mergedOptions.authToken,
-                "subjectTokenType": "urn:ietf:params:oauth:token-type:jwt",
-                "scope": "https://www.googleapis.com/auth/cloud-platform"
-            }
-        )
-    }).then(res => res.json()).then(res => res.access_token)
-    return await fetch(`https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${mergedOptions.serviceAccount}:generateIdToken`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${stsToken}`
-        },
-        body: JSON.stringify({
-            "audience": mergedOptions.iamAudience,
-            "includeEmail": "true"
+            grantType: 'urn:ietf:params:oauth:grant-type:token-exchange',
+            audience: mergedOptions.stsAudience,
+            requestedTokenType: 'urn:ietf:params:oauth:token-type:access_token',
+            subjectToken: mergedOptions.authToken,
+            subjectTokenType: 'urn:ietf:params:oauth:token-type:jwt',
+            scope: 'https://www.googleapis.com/auth/cloud-platform'
         })
-    }).then(res => res.json()).then(res => res.token)
+    }).then(async res => await res.json()).then(res => res.access_token)
+    return await fetch(`https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${mergedOptions.serviceAccount}:generateIdToken`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${stsToken}`
+        },
+        body: JSON.stringify({
+            audience: mergedOptions.iamAudience,
+            includeEmail: 'true'
+        })
+    }).then(async res => await res.json()).then(res => res.token)
+}
+
+export interface MergedOptions {
+    authToken: string
+    stsAudience: string
+    serviceAccount: string
+    stsEndpoint: string
+    iamAudience: string
 }
 
 export interface Options {
-    authToken: string,
-    stsAudience: string,
-    serviceAccount: string,
-    stsEndpoint?: string,
-    iamAudience? : string
+    authToken: string
+    stsAudience: string
+    serviceAccount: string
+    stsEndpoint?: string
+    iamAudience?: string
 }
 
 export const defaultOptions = {
-    stsEndpoint: "https://sts.googleapis.com/v1/token",
-    iamAudience: "32555940559.apps.googleusercontent.com"
+    stsEndpoint: 'https://sts.googleapis.com/v1/token',
+    iamAudience: '32555940559.apps.googleusercontent.com'
 }
 
 export default getAccessToken
